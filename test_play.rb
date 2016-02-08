0e70438a59bfcf5a294367af2b097d8462ffcd22
IzikAJ <izikaj@gmail.com>
2016-02-08 21:36:08 +0200

    upd uploader

--- test_play.rb
+++ test_play.rb
@@ -14,31 +14,37 @@ mech = Mechanize.new { |agent|
   agent.user_agent_alias = 'Mac Safari'
 }
 
-def download file_url, filename=nil, chunk=1<<12, &block
+def download file_url, filename=nil, chunk=1<<15, &block
   path, filename = File.split(file_url) if filename.nil?
   total = 0
-  last = -1
+  last = 0
+  _last = Time.now.to_f
   stream = open( file_url,
     content_length_proc: proc { |length| total = length },
     progress_proc: proc do |step|
-      if block_given? && (last < 0) || ((step - last)>1024000)
-        block.call(step, total)
+      if block_given? && (last <= 0) || ((step - last)>chunk)
+        _now = Time.now.to_f
+        speed = (step-last)/(_now-_last)
+        block.call(step, total, speed)
         last = step
+        _last = _now
       end
     end
   )
+  block.call(total, total, 0) if block_given?
   IO.copy_stream(stream, filename)
 end
 
-mech.get("http://rwpod.podfm.ru") do |page|
-end
-
-# file = 'http://rwpod.podfm.ru/my/140/file/podfm_rwpod_my_4_140.mp3'
-# path, name = File.split file
-
-# download(file) do |step, total|
-#   puts "Downloaded #{step>>10}kb / #{total>>10}kb"
+# mech.get("http://rwpod.podfm.ru") do |page|
 # end
+
+file = 'http://rwpod.podfm.ru/my/140/file/podfm_rwpod_my_4_140.mp3'
+# path, name = File.split file
+path, filename = File.split(file)
+
+download(file, File.join("out", filename)) do |step, total, speed|
+  puts "Downloaded #{step>>10}kb / #{total>>10}kb at #{speed/1024}kb/s"
+end
 # 
 # mech.download(file, name)
 
